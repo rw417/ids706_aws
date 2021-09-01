23c4bb2a145414948cdc747df0a5960c9831428e
Robert Wan <37159376+rw417@users.noreply.github.com>
2021-09-01 17:49:36 +0000

    changed welcome msg to test CD

--- project1.py
+++ project1.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"Welcome Message": "Hello World"}
+    return {"Welcome Message": "Hello World 1:47pm 9/1"}
 
 
 @app.get("/add/{num1}/{num2}")
