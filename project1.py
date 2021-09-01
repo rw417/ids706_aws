82ec6c5da78ce23e53e79063cecd67acfebb8e45
Robert Wan <37159376+rw417@users.noreply.github.com>
2021-09-01 17:07:09 +0000

    changes

--- project1.py
+++ project1.py
@@ -13,17 +13,18 @@ async def root():
 @app.get("/add/{num1}/{num2}")
 async def add(num1: int, num2: int):
     """add two numbers together"""
-    
+
     total = num1 + num2
     return {"total is": total}
+
 
 @app.get("/multiply/{num1}/{num2}")
 async def multiply(num1: int, num2: int):
     """add two numbers together"""
-    
+
     total = num1 * num2
     return {"product is": total}
 
-    
-if __name__ == '__main__':
-    uvicorn.run(app, port=8080, host='0.0.0.0')
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0", reload=True)
