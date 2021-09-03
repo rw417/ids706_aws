9ada481e9ac78cbb1ae4be117a8b69a8febe4096
Robert Wan <37159376+rw417@users.noreply.github.com>
2021-09-03 21:04:44 +0000

    added minus function to FastAPI

--- project1.py
+++ project1.py
@@ -24,6 +24,13 @@ async def multiply(num1: int, num2: int):
     total = num1 * num2
     return {"product is": total}
 
+@app.get("/minus/{num1}/{num2}")
+async def minus(num1: int, num2: int):
+    """num1 minus num2"""
+
+    diff = num1 - num2
+    return {"difference is": diff}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
